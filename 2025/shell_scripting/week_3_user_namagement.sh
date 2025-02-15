7e1972780fc10563e2a1eec3031c6ecb48cf443b
niladri26 <105287219+niladri26@users.noreply.github.com>
2025-02-15 17:39:35 +0530

    Update week_3_user_namagement.sh

--- 2025/shell_scripting/week_3_user_namagement.sh
+++ 2025/shell_scripting/week_3_user_namagement.sh
@@ -41,3 +41,112 @@ fi
 
 #create_user
 
+# 2. delete account
+
+function delete_user {
+
+read -p "Enter the username you want to delete: " username
+
+if [ $(cat /etc/passwd | grep $username | wc | awk '{print $1}') -eq 0 ];
+then
+
+        echo "user doesnot exists! "
+
+else
+
+        sudo userdel $username
+
+        echo "======== user deleted successfully ==========="
+
+fi
+
+}
+
+#delete_user
+
+
+# 3. Reset password
+
+function reset_password {
+
+read -p "Enter the username to update password: " username
+
+if [ $(cat /etc/passwd | grep $username | wc | awk '{print $1}') == 0 ];
+then
+
+        echo "user doesnot exists! Please enter correct username!"
+
+else
+
+        # reset password
+
+        read -p "Enter password: " password
+
+        echo -e "$password\n$password" | sudo passwd "$username" > /dev/null
+
+
+        echo "password reset successfully! ";
+
+fi
+
+}
+
+#reset_password
+
+
+# 4. List user accounts
+
+function list_users {
+
+        awk -F':' '{print $1}' /etc/passwd
+
+}
+
+
+#list_users
+
+
+# 5. Help and usage information
+
+
+function help {
+
+        echo -e "\n\n ======== CHOOSE AN OPTION FROM BELOW ========
+                 Enter -c or --create to create a new user \n\n
+                 Enter -d or --delete to delete an existing user \n\n
+                 Enter -r or --reset to reset password \n\n
+                 Enter -l or --list to display all the users \n\n
+                 Enter -h or --help to display help and usage information \n\n"   
+}
+
+help
+
+
+
+read option
+
+if [[ "$option" == "-c" || "$option" == "--create" ]];
+then
+        create_user
+
+elif [[ "$option" == "-d" || "$option" == "--delete" ]];
+then
+        delete_user
+
+elif [[ "$option" == "-r" || "$option" == "--reset" ]];
+then
+        reset_password
+
+elif [[ "$option" == "-l" || "$option" == "--list" ]];
+then
+        list_users
+
+elif [[ "$option" == "-h" || "$option" == "--help" ]];
+then
+       help
+
+fi
+
+
+
+
